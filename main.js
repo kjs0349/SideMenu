4bc7bdcd16bd94ce37f4d231cfc628cf9f3b22c1
Jisu <kjl0349@naver.com>
2021-01-08 00:31:19 +0900

    필요없는 주석 코드 삭제 및 완성

--- main.js
+++ main.js
@@ -6,12 +6,10 @@ const label = document.querySelector('label');
 bookBtn.addEventListener('click', function() {
     section.style.left = 0;
     label.style.opacity = 0;
-    // label.style.transitionDelay = 0;
 })
 
 //포커스 아웃 시 사이드 메뉴 사라짐, 북마크 버튼 생성
 bookBtn.addEventListener('blur', function() {
     section.style.left = '-200px';
     label.style.opacity = 1;
-    // label.style.transitionDelay = '.5s';
 })
